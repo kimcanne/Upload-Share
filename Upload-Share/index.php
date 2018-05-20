94392eb344a73f794855bac8583f2b60749b384a
i-f-t-i <31423256+i-f-t-i@users.noreply.github.com>
2018-05-20 11:33:24 -0400

    updated index.php

--- Upload-Share/index.php
+++ Upload-Share/index.php
@@ -7,143 +7,150 @@ if(file_exists("install/index.php")){
 }
 
 require_once 'users/init.php';
-require_once $abs_us_root.$us_url_root.'users/includes/header.php';
-require_once $abs_us_root.$us_url_root.'users/includes/navigation.php';
+
+
+
 if(isset($user) && $user->isLoggedIn()){
 }
 ?>
 
-<div id="page-wrapper">
-<div class="container">
-<div class="row">
-	<div class="col-xs-12">
+<!DOCTYPE html>
+<html lang="en">
 
-		<div class="jumbotron">
-			<h1>Welcome to <?php echo $settings->site_name;?></h1>
-			<p class="text-muted">An Open Source PHP User Management Framework. </p>
-			<p>
-			<?php if($user->isLoggedIn()){$uid = $user->data()->id;?>
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Business Frontpage - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="users/upload/index_files/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="users/upload/index_files/css/business-frontpage.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Upload & Share</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+
+              	<?php if($user->isLoggedIn()){$uid = $user->data()->id;?>
 				<a class="btn btn-default" href="users/account.php" role="button">User Account &raquo;</a>
 			<?php }else{?>
 				<a class="btn btn-warning" href="users/login.php" role="button">Log In &raquo;</a>
 				<a class="btn btn-info" href="users/join.php" role="button">Sign Up &raquo;</a>
 			<?php } ?>
-			</p>
-		</div>
-	</div>
-</div>
-<div class="row">
-<?php
-// To generate a sample notification, uncomment the code below.
-// It will do a notification everytime you refresh index.php.
-// $msg = 'This is a sample notification! <a href="'.$us_url_root.'users/logout.php">Go to Logout Page</a>';
-// $notifications->addNotification($msg, $user->data()->id);
- ?>
-<div class="col-md-6">
-	<div class="panel panel-default">
-		<div class="panel-heading"><strong>Step 1: Change your password!</strong></div>
-		<div class="panel-body">You're going to login with the default username of <strong>admin</strong> and the default password of <strong>password</strong>.
-		You can also login as a standard level user with the credentials of <strong>user</strong> and <strong>password</strong>.
-		If you cannot login for some reason, edit the login.php file and uncomment out the lines<br> error_reporting(E_ALL);<br>
-		ini_set('display_errors', 1);<br> to see if there are any errors in your server configuration.
-		</div>
-	</div><!-- /panel -->
-</div><!-- /.col -->
-<div class="col-md-6">
-	<div class="panel panel-default">
-		<div class="panel-heading"><strong>Step 2: Change some settings</strong></div>
-		<div class="panel-body">You want to go to the Admin Dashboard. From there you can personalize your settings.
-		You can decide whether or not you want to use reCaptcha, force SSL, or mess with some CSS.
-		</div>
-	</div><!-- /panel -->
-</div><!-- /.col -->
-</div><!-- /.row -->
+            
 
-<div class="row">
-<div class="col-md-6">
-	<div class="panel panel-default">
-		<div class="panel-heading"><strong>Step 3: Explore</strong></div>
-		<div class="panel-body">From the Admin Dashboard, you can go to Admin Permissions and add some new user levels.
-		Then check out Admin Pages to decide which pages are private and which are public. Once you make a page private,
-		you can decide how what level of access someone needs to access it.
-		Any new pages you create in your site folder will automatically show up here.
-		</div>
-	</div><!-- /panel -->
-</div><!-- /.col -->
-<div class="col-md-6">
-	<div class="panel panel-default">
-		<div class="panel-heading"><strong>Step 4: Check out the other resources</strong></div>
-		<div class="panel-body">The users/blank_pages folder contains a blank version of this page and one with the sidebar
-		included for your convenience. There are also special_blanks that you can drop into your site folder and load up to
-		check out all the things you can do with Bootstrap.
-		</div>
-	</div><!-- /panel -->
-</div><!-- /.col -->
-</div><!-- /.row -->
+          </ul>
+        </div>
+      </div>
+    </nav>
 
-<div class="row">
-<div class="col-md-6">
-	<div class="panel panel-default">
-		<div class="panel-heading"><strong>Step 5: Design and secure your own pages</strong></div>
-		<div class="panel-body">Of course, using our blanks is the quickest way to get up and running,
-		but you can also secure any page. Simply add this php code to the top of your page and it will
-		perform a check to see if you've set any special permissions.<br/>
-		require_once 'users/init.php';<br/>
-		require_once $abs_us_root.$us_url_root.'users/includes/header.php';<br/>
-		require_once $abs_us_root.$us_url_root.'users/includes/navigation.php';<br/>
-		  if (!securePage($_SERVER['PHP_SELF'])){die();}
-		</div>
-	</div><!-- /panel -->
-</div><!-- /.col -->
-<div class="col-md-6">
-	<div class="panel panel-default">
-		<div class="panel-heading"><strong>Step 6: Check out the forums and documentation at <a target="_blank" href="http://UserSpice.com">UserSpice.com</strong></a></div>
-		<div class="panel-body">That's where the latest options are and you can find people willing to help.
-		No account is required for browsing the forums, but you will need to sign up to be able to post.
-		</div>
-	</div><!-- /panel -->
-</div><!-- /.col -->
-</div><!-- /.row -->
+    <!-- Header with Background Image -->
+    <header class="business-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
 
-<div class="row">
-<div class="col-md-6">
-	<div class="panel panel-default">
-		<div class="panel-heading"><strong>Step 7: Replace this ugly homepage with your own beautiful creation</strong></div>
-		<div class="panel-body">Don't forget to swap out logo.png in the images folder with your own! If you're getting nagging
-		message in the footer, <a href="https://www.google.com/recaptcha/admin#list">go get you some of your own reCAPTCHA keys</a>
-		</div>
-	</div><!-- /panel -->
-</div><!-- /.col -->
-<div class="col-md-6">
-	<div class="panel panel-default">
-		<div class="panel-heading"><strong>Step 8: Avoid editing the UserSpice files</strong></div>
-		<div class="panel-body">But what if you want to change the UserSpice files?
-		We have a solution that lets you edit our files and still not break future upgrades.
-		For instance, if you want to modify the account.php file... just copy our file into
-		the "usersc" folder.  Then you can edit away and your file will be loaded instead of ours!
-		</div>
-	</div><!-- /panel -->
-</div><!-- /.col -->
-</div><!-- /.row -->
+            
+             
+            <h1 class="display-3 text-center text-white mt-4">Upload & Share</h1>
 
-<div class="row">
-<div class="col-xs-12">
-	<div class="well"><p>UserSpice is built using <a href="http://getbootstrap.com/">Twitter's Bootstrap</a>,
-	so it is fully responsive and there is tons of documentation. The look and the feel can be changed very easily. </p>
-	<p>Consider checking out <a href="http://bootsnipp.com">Bootsnipp</a> to see all the widgets and tools you can
-	easily drop into UserSpice to get your project off the ground.
-	</div>
-</div><!-- /.col -->
-</div><!-- /.row -->
+            
+              
+          </div>
+        </div>
+      </div>
+    </header>
 
-</div> <!-- /container -->
+    <!-- Page Content -->
+    <div class="container">
 
-</div> <!-- /#page-wrapper -->
+      <!-- /.row -->
 
-<!-- footers -->
-<?php require_once $abs_us_root.$us_url_root.'users/includes/page_footer.php'; // the final html footer copyright row + the external js calls ?>
+      <div class="row">
+        <div class="col-sm-4 my-4">
+          <div class="card">
+            <img class="card-img-top" src="users/upload/index_files/css/7.jpg" alt="">
+            <div class="card-body">
+              <h4 class="card-title text-center">Create</h4>
+              <p class="card-text text-center">Create your online folders for files.</p>
+            </div>
+            <div class="card-footer text-center">
+              <a href="users/join.php" class="btn btn-primary">Find Out More!</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4 my-4">
+          <div class="card">
+            <img class="card-img-top" src="users/upload/index_files/css/OLAZ660.jpg" alt="">
+            <div class="card-body">
+              <h4 class="card-title text-center">Upload</h4>
+              <p class="card-text text-center">Upload your files to your own private or public folders online.</p>
+            </div>
+            <div class="card-footer text-center">
+              <a href="users/join.php" class="btn btn-primary">Find Out More!</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4 my-4">
+          <div class="card">
+            <img class="card-img-top" src="users/upload/index_files/css/119473-OPL22L-840-min.jpg" alt="">
+            <div class="card-body">
+              <h4 class="card-title text-center">Access</h4>
+              <p class="card-text text-center">Limit folder access to private or public</p>
+            </div>
+            <div class="card-footer text-center">
+              <a href="users/join.php" class="btn btn-primary">Find Out More!</a>
+            </div>
+          </div>
+        </div>
 
-<!-- Place any per-page javascript here -->
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+
+          
+
+        <div class="row">
+                <div class="col-sm-12 text-center">
+                        <footer><font color='white'><br>&copy;
+                          <?php echo date("Y"); ?>
+                           <?=$settings->copyright; ?></font></footer>
+                        <br>
+                </div>
+        </div>
 
 
-<?php require_once $abs_us_root.$us_url_root.'users/includes/html_footer.php'; // currently just the closing /body and /html ?>
+       <p   <a class="m-0 text-center text-white" href="https://www.freepik.com/free-vector/uploading-background-design_1050692.htm">Graphics by Freepik</a> </p>
+            
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="users/upload/index_files/vendor/jquery/jquery.min.js"></script>
+    <script src="users/upload/index_files/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
