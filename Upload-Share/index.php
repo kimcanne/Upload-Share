11611ead16a0bce475afed480abf03b96aef1b55
i-f-t-i <31423256+i-f-t-i@users.noreply.github.com>
2019-03-20 07:14:20 -0400

    Update index.php

--- Upload-Share/index.php
+++ Upload-Share/index.php
@@ -30,7 +30,9 @@ if(isset($user) && $user->isLoggedIn()){
         <!-- Navigation -->         
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"> 
             <div class="container"> 
-                <a class="navbar-brand" href="#">Upload & Share</a> 
+                <a class="navbar-brand" href="#">
+                    <img class="img-fluid" src="users/images/logo.png">
+                </a>                 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"> 
                     <span class="navbar-toggler-icon"></span> 
                 </button>                 
