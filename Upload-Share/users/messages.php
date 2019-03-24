def12ccc059db5fc54c99360170db65781c26393
i-f-t-i <31423256+i-f-t-i@users.noreply.github.com>
2019-03-24 18:55:17 -0400

    Update messages.php

--- Upload-Share/users/messages.php
+++ Upload-Share/users/messages.php
@@ -22,8 +22,8 @@ Special thanks to user Brandin for the mods!
 ?>
 <?php
 require_once 'init.php';
-require_once $abs_us_root.$us_url_root.'users/includes/header.php';
-require_once $abs_us_root.$us_url_root.'users/includes/navigation.php';
+require_once $abs_us_root.$us_url_root.'users/includes/custom_header.php';
+require_once $abs_us_root.$us_url_root.'users/includes/custom_navigation.php';
 ?>
 
 <?php if (!securePage($_SERVER['PHP_SELF'])){die();}
