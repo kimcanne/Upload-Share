b4f6e47d319dffc086776339da10329001c2dd32
i-f-t-i <31423256+i-f-t-i@users.noreply.github.com>
2019-03-26 22:39:16 -0400

    Revert "Update cleanup.php"
    This reverts commit 6958c3c9ad636997839a8455ebaf256c283c587f.

--- Upload-Share/install/cleanup.php
+++ Upload-Share/install/cleanup.php
@@ -8,9 +8,12 @@ foreach ($files as $file) {
 	}
 }
 rrmdir("install");
-?> </p> 
-<p align="center">If you made it this far, everything SHOULD be good to go. If you see any errors above, you will want to navigate to the install folder, and delete it manually. Click the button below to make sure you have the latest updates to your database.</p> 
-<h3 align="center"><a class="button" href="..index.php">Lets look at your website!</a></h3> 
+?>
+</p>
+<p align="center">If you made it this far, everything SHOULD be good to go. If you see any errors above, you will want to navigate to the install folder, and delete it manually. Click the button below to make sure you have the latest updates to your database.</p>
+
+
+<h3 align="center"><a class="button" href="../users/update.php">Update Database and Login!</a></h3>
 <?php
 function rrmdir($dir) {
   if (is_dir($dir)) {
